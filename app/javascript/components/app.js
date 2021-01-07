4a555c52ce87576bd2ea8c71fe4ec2743da14940
matt_kuras <mattkuras@gmail.com>
2021-01-07 14:39:22 -0500

    congifure webpacker and react

--- app/javascript/components/app.js
+++ app/javascript/components/app.js
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div className='app'>
-
+            hello world
         </div>
       );
 }
--- app/javascript/packs/index.js
+++ app/javascript/packs/index.js
@@ -4,7 +4,7 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app.js'
+import App from '../components/app.js'
 
 
 document.addEventListener('DOMContentLoaded', () => {

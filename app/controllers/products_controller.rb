33539fdb7daef2cba20c5cae38509db16a9fe7d5
matt_kuras <mattkuras@gmail.com>
2021-01-10 17:31:32 -0500

    generate and migrate product table, create model and routes for index/create

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,28 @@
 class ProductsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+    def index
+        products = Product.all.first 
+        render json: products
+    end
 
     def create
-        
+        product = Product.new(product_params)
+        if product.save
+            render json: product 
+        else
+            render json: "there was an error creating your product"
+        end
+    end
+
+    private 
+    def product_params
+        params.require(:product).permit(
+            :name,
+            :price,
+            :category,
+            :image
+        )
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get '*path', to: 'home#index', via: :all
   post 'charges/create', to: 'charges#create'
+  post 'products', to: 'products#create'
+  get 'products', to: 'products#index'
+  get '*path', to: 'home#index', via: :all
 end

0fdb71711d36f84d28cfc9aa4da79ac60a7a73d3
matt_kuras <mattkuras@gmail.com>
2021-01-11 13:22:14 -0500

    confiure keys and credentials for aws and edited dev and prod environments

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
     def index
-        products = Product.all.first 
+        products = Product.all
         render json: products
     end
 

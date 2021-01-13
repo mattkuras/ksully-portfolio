145b9b146abf446e52c43bf5183c184ac3515139
Jesse McMahon <Jessemcmahon17@gmail.com>
2021-01-13 11:14:19 -0500

    add functionality to get products from backend and display on dashboard page. Also added styling for products and dashboard page

--- app/javascript/src/components/Admin/AdminDashboard.js
+++ app/javascript/src/components/Admin/AdminDashboard.js
@@ -1,22 +1,49 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+import {Link} from "react-router-dom"
 import "./Admin.css"
 import { DirectUpload } from 'activestorage';
 
 const Admin = () => {
+    const [productList, setProductList] = useState([]);
     const [productImage, setProductImage] = useState('');
     const [productName, setProductName] = useState('');
     const [productPrice, setProductPrice] = useState(0);
     const [productCategory, setProductCategory] = useState('');
 
+    useEffect(() => {
+      const response = fetch('http://localhost:3000/products')
+      .then(response => response.json())
+      .then(data => setProductList(data));
+    }, [])
+
+   const Gallery = () => {
+    return<div className="product-container">
+       {productList.map((product) => (
+         <div key={product.name} className="product">
+           <img src="https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8bW91bnRhaW5zfGVufDB8fDB8&ixlib=rb-1.2.1&w=1000&q=80"/>
+           <h1>{product.name}</h1>
+           <h3>Price: ${product.price/100}</h3>
+           <h3>Category: {product.category}</h3>
+           <div className="btn-container"> 
+              <button className="btn edit-btn">Edit</button>
+              <button className="btn delete-btn">Delete</button>
+           </div>
+           </div>
+       ))}
+        </div>
+     }
+
+
     const handleFormSubmit = (e) => {
         e.preventDefault()
         createProduct()
+        window.location.reload(false)
     }
 // creates product then calls uploadfile
    const createProduct = async () => {
     let product = {
         name: productName,
-        price: productPrice,
+        price: productPrice * 100,
         category: productCategory
       }
         fetch('/products', {
@@ -51,8 +78,12 @@ const Admin = () => {
 
     return (
         <div className="admin-page-container">
-            <h1>Admin Dashboard</h1>
+            <div className="header">
+              <h2 className="header-item">Admin Dashboard</h2>
+              <Link className="link-to-shop header-item" to="/shop"><h1>Go To Shop</h1></Link>
+            </div>
             <form onSubmit={handleFormSubmit} className="product-form">
+              <h1>Enter New Product</h1>
                 <input onChange={e => setProductImage(e.target.files[0]) } className="product-info" type="file" />
                 <input value={productName} onChange={(e) => setProductName(e.target.value) }  className="product-info" type="text" placeholder="Enter Product Name Here" />
                 <input value={productPrice} onChange={e => setProductPrice(e.target.value) }  className="product-info" type="text" placeholder="Enter Product Price Here" />
@@ -66,6 +97,9 @@ const Admin = () => {
                 </select>
                 <input type="submit" />
             </form>
+           
+              <Gallery />
+           
         </div>
     )
 }

b8cd3343ddc5d599d7e51f6236340460bf5d01e9
matt_kuras <mattkuras@gmail.com>
2021-01-20 14:37:23 -0500

    add back drop functionality when product is clicked

--- app/javascript/src/components/Shop/Shop.js
+++ app/javascript/src/components/Shop/Shop.js
@@ -1,50 +1,51 @@
 import React, { useState, useEffect } from "react"
 import { loadStripe } from "@stripe/stripe-js"
 import "./Shop.css"
-
+import Backdrop from '../backdrop/backdrop.js'
 const stripePromise = loadStripe('pk_test_51I73MsKw5OX78tFumdDo0CLhysi8WPejrlcjAjqIvYRo1dAOAr0kFGiFXVUXph8T2RXjv6ikYWCZyKKLDt7Fhj6t00DTnSFBMD')
 
 
 function Shop() {
   const [productList, setProductList] = useState([]);
+  const [showOpen, setShowOpen] = useState(false)
+
 
   useEffect(() => {
     const response = fetch('http://localhost:3000/products')
-    .then(response => response.json())
-    .then(data => setProductList(data));
+      .then(response => response.json())
+      .then(data => setProductList(data));
   }, [])
 
   const Gallery = () => {
     console.log(productList)
     return <section className="products">
-       {productList.map((product, index) => (
-         <div key={product.name} className="product-card">
-           <div className="product-image">
-              <img src={product.image_url} />
-              <img src="https://i.pinimg.com/originals/81/c3/3a/81c33af00010a4b7a05b3f35cb9943b9.jpg" />
+      {productList.map((product, index) => (
+        <div key={product.name} className="product-card" onClick={showPageClickHandler}>
+          <div className="product-image">
+            <img src={product.image_url} />
+            <img src="https://i.pinimg.com/originals/81/c3/3a/81c33af00010a4b7a05b3f35cb9943b9.jpg" />
           </div>
           <div className="product-info">
-               <h2 className="product-title">{product.name}</h2>
-               <h4 className="product-price">${product.price/100}.00</h4>
-               <button value={index} onClick={handleClick} className="checkout-button">Buy Now</button>
-               {/* <button value={product.id} onClick={initializeShowProduct} className="button enlarge-btn">Enlarge Image</button> */}
+            <h2 className="product-title">{product.name}</h2>
+            <h4 className="product-price">${product.price / 100}.00</h4>
+            <button value={index} onClick={handleClick} className="checkout-button">Buy Now</button>
+            {/* <button value={product.id} onClick={initializeShowProduct} className="button enlarge-btn">Enlarge Image</button> */}
           </div>
         </div>
-       ))}
-        </section>
-     }
- 
-  const handleClick = async (e) => {
-// dummy data for request
-  const index = e.target.value
-
-  const charge = {
-    name: productList[index].name,
-    image: productList[index].image_url,
-    unit_amount: productList[index].price,
-    quantity: 1
+      ))}
+    </section>
   }
 
+  const handleClick = async (e) => {
+    // dummy data for request
+    const index = e.target.value
+
+    const charge = {
+      name: productList[index].name,
+      image: productList[index].image_url,
+      unit_amount: productList[index].price,
+      quantity: 1
+    }
     //Get Instance of Stripe
     const stripe = await stripePromise;
 
@@ -52,7 +53,7 @@ function Shop() {
     const response = await fetch('/charges/create', {
       method: 'POST',
       headers: {
-          'Content-Type': 'application/json'
+        'Content-Type': 'application/json'
       },
       body: JSON.stringify(charge)
     });
@@ -62,44 +63,57 @@ function Shop() {
     const result = await stripe.redirectToCheckout({
       sessionId: session.id,
     });
-
     if (result.error) {
       alert("Something went wrong, check the console")
       console.log(result.error)
     }
-  
+  }
+
+  const showPageClickHandler = () => {
+    setShowOpen(true)
+  }
+  const backdropClickHandler = () => {
+    setShowOpen(false)
+    console.log('clicked')
+  }
+  let backdrop;
+
+  if (showOpen) {
+    backdrop = <Backdrop close={backdropClickHandler} />
   }
 
   return (
+
     <div className="shop-page-container">
-    <div className="shop-page-content">
-    <nav className="product-filter">
-    <h1>Kyle Sullivan Visual</h1>
-      <div className="collection-sort">
-        <label>Categories:</label>
-        <select>
-          <option value="/">All Photos</option>
-        </select>
+      <div className="shop-page-content">
+        <nav className="product-filter">
+          <h1>Kyle Sullivan Visual</h1>
+          <div className="collection-sort">
+            <label>Categories:</label>
+            <select>
+              <option value="/">All Photos</option>
+            </select>
+          </div>
+        </nav>
+
+        <section className="products">
+
+          <div className="product-card">
+            <div className="product-image">
+              <img src="https://i0.wp.com/theluxurytravelexpert.com/wp-content/uploads/2019/01/zakynthos-JPEG.jpg?fit=1920%2C1080&ssl=1" />
+            </div>
+            <div className="product-info">
+              <h2 className="product-title">Montego Bay</h2>
+              <h4 className="product-price">$99.99</h4>
+              <button onClick={handleClick} id="checkout-button">Buy Now</button>
+            </div>
+          </div>
+          <Gallery />
+
+        </section>
       </div>
-  </nav>
-
-  <section className="products">
-
-  <div className="product-card">
-    <div className="product-image">
-      <img src="https://i0.wp.com/theluxurytravelexpert.com/wp-content/uploads/2019/01/zakynthos-JPEG.jpg?fit=1920%2C1080&ssl=1" />
+      {backdrop}
     </div>
-    <div className="product-info">
-      <h2 className="product-title">Montego Bay</h2>
-      <h4 className="product-price">$99.99</h4>
-      <button onClick={handleClick} id="checkout-button">Buy Now</button>
-    </div>
-  </div>
-  <Gallery />
-  
-</section>
-  </div>
-  </div>
   );
 }
 

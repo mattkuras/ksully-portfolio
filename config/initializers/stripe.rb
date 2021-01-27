ad41601dd4fd1acfc037ed23f332abc75c648a86
matt_kuras <mattkuras@gmail.com>
2021-01-26 21:50:07 -0500

    more stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 Rails.configuration.stripe = {
     :publishable_key => Rails.application.credentials.dig(:stripe, :stripe_publishable_key),
-    :secret_key => Rails.application.credentials.dig(:stripe, :stripe_api_key)
+    :stripe_api_key => Rails.application.credentials.dig(:stripe, :stripe_api_key)
 
 }
 
-Stripe.api_key = Rails.application.credentials.stripe[:stripe_api_key]
-# Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = Rails.application.credentials.stripe[:stripe_api_key]
+Stripe.api_key = Rails.configuration.stripe[:stripe_api_key]

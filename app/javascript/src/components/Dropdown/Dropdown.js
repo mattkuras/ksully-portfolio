f71ab9f26b496e1d5e19b565baaf11e0fda81555
matt_kuras <mattkuras@gmail.com>
2021-02-02 10:12:36 -0500

    add some links to drop down run bundle merge with show page fix

--- app/javascript/src/components/Dropdown/Dropdown.js
+++ app/javascript/src/components/Dropdown/Dropdown.js
@@ -5,10 +5,10 @@ import {AiOutlineClose} from "react-icons/ai"
 const Dropdown = props => (
     <div className='dropdown'>
             <AiOutlineClose className="close-dropdown" onClick={props.close} />
-            <a>Homepage</a>
+            <a href='/'>Homepage</a>
             <a>Contact</a>
             <a>FAQs</a>
-            <a>Gallery</a>
+            <a href='/shop'>Shop</a>
         </div>
 )
 

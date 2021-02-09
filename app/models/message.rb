bae9c5a54343d40c1be5a6822b3e50f2c3129fa5
matt_kuras <mattkuras@gmail.com>
2021-02-08 20:25:00 -0500

    fixed time methods for messages

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,8 +4,9 @@ class Message < ApplicationRecord
     end
 
     def time_or_day
-        t = created_at.getlocal
+        t = created_at
         if t.today?
+            t = t.getlocal
             h = t.hour
             m = t.min
             if t.hour > 12
@@ -18,6 +19,7 @@ class Message < ApplicationRecord
             time = t.ctime.slice(0..9)
         end
     end
+
 
 
     def datetime
@@ -33,6 +35,16 @@ class Message < ApplicationRecord
         else 
             time = h.to_s + ':' + m.to_s + 'am'
             time + ' ' + day         
+        end
+    end
+
+    private
+
+    def add_zero(num)
+        if num.to_a.length == 1
+            result = '0' + num.to_s 
+        else
+            result = num.to_s
         end
     end
 
